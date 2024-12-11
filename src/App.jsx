48e5f25c6d0a6ce5cc0f48729525b519a22e9d98
Siraj <ss1425306@gmail.com>
2024-12-11 17:52:25 +0530

    first commit- add structure and code  Login

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Login from './Components/Auth/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <Login/>
+      </div>    
+  </>
+  )
+}
+
+export default App
---
+++ src/Components/Auth/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submitHandler =(e)=>{
+        e.preventDefault()
+        console.log(`Email:${email}`)
+        console.log(`Password: ${password}`)
+        setEmail('')
+        setPassword('')
+     }
+
+  return (
+    <div className='flex h-screen w-screen items-center justify-center '>
+        <div className='border-2 border-emerald-600 p-20 rounded-xl'>
+            <form onSubmit={submitHandler}
+            className='flex flex-col items-center justify-center'>
+                <input type="email" required
+                onChange={(e)=>{
+                    setEmail(e.target.value)
+                }}
+                value={email}
+                className=' outline-none bg-transparent border-2 border-emerald-600 py-4 px-5 text-xl rounded-full 
+                placeholder:text-blue-300'
+                 placeholder='Enter Your Email'/>
+                
+                <input type="password" required
+                onChange={(e)=>{
+                    setPassword(e.target.value)
+                }}
+                value={password}
+                className=' outline-none bg-transparent border-2 border-emerald-600 py-4 px-5 text-xl rounded-full 
+                placeholder:text-blue-300 mt-3'
+                placeholder='Enter Your Password' />
+                
+                <button className='text-white outline-none  border-none bg-emerald-600 py-4 px-5 text-xl rounded-full 
+                placeholder:text-blue-100 mt-5'
+                >Login</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
