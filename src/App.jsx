faa2d61f2232910846aeebb865c4dbd772cbee5b
Siraj <ss1425306@gmail.com>
2024-12-13 21:27:01 +0530

    setup role

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,24 @@ function App() {
   const authData= useContext(AuthContext)
   // console.log(authData)
 
+  useEffect(()=>{
+    if (authData) {
+      const loggedInUser= localStorage.getItem('loggedInUser') 
+        if (loggedInUser) {
+         setUser(loggedInUser.role) 
+        }
+    }
+  },[authData])
+
   const handleLogin=(email,password)=>{
     if (email=='admin@me.com' && password == '123') {
       setUser('admin')
       // console.log(user)
+      localStorage.setItem("loggedInUser",JSON.stringify({role: 'admin'}))
     } else if(authData && authData.employees.find((e)=>email == e.email && e.password== password)){
       setUser('employee')
       // console.log(user)
+      localStorage.setItem("loggedInUser",JSON.stringify({role: 'employee'}))
     }
     else{
       alert('invalid credential')
