bc68374e9d8bd715aad06182b2403da5f2f487f1
Siraj <ss1425306@gmail.com>
2024-12-18 18:06:36 +0530

    update bsed on eacn comp

--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -1,46 +1,30 @@
 import React from 'react'
+import AcceptTask from './AcceptTask'
+import NewTask from './NewTask'
+import CompletedTask from './CompletedTask'
+import FailedTask from './FailedTask'
 
-const TaskList = () => {
+const TaskList = ({loggedInUserData}) => {
+    console.log(loggedInUserData);
+    
   return (
-    <div id='tasklist' className='h-[55%] overflow-x-auto scrollbar-hide flex items-center justify-start gap-5 w-full py-5 mt-10 text-left'>
-        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
-            <div className='flex justify-between items-center '>
-                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                <h4 className='text-sm'>24 Feb 2024</h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-semibold '>Make Youtube Video</h2>
-            <p className='text-sm mt-2'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum temporibus distinctio minus, id cumque natus!</p>
-        </div>
-        
-        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl'>
-            <div className='flex justify-between items-center '>
-                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                <h4 className='text-sm'>24 Feb 2024</h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-semibold '>Make Youtube Video</h2>
-            <p className='text-sm mt-2'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum temporibus distinctio minus, id cumque natus!</p>
-        </div>
-        
-        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl'>
-            <div className='flex justify-between items-center '>
-                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                <h4 className='text-sm'>24 Feb 2024</h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-semibold '>Make Youtube Video</h2>
-            <p className='text-sm mt-2'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum temporibus distinctio minus, id cumque natus!</p>
-        </div>
-        
-        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl'>
-            <div className='flex justify-between items-center '>
-                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                <h4 className='text-sm'>24 Feb 2024</h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-semibold '>Make Youtube Video</h2>
-            <p className='text-sm mt-2'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum temporibus distinctio minus, id cumque natus!</p>
-        </div>
-        
-
-        
+    <div  id='tasklist' className='h-[55%] overflow-x-auto scrollbar-hide flex items-center justify-start gap-5 w-full py-5 mt-10 text-left'>
+       {loggedInUserData.tasks.map((el,index)=>{
+        console.log(el)
+        if (el.status.active){
+            return <AcceptTask key={index}/>
+        }
+        if (el.status.newTask){
+            return <NewTask key={index}/>
+        }
+        if (el.status.completed){
+            return <CompletedTask key={index}/>
+        }
+        if (el.status.failed){
+            return <FailedTask key={index}/>
+        }
+       }
+       )}
     </div>
   )
 }
