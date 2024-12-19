1a4110cbc482cb129cf7460c4a0584575a6c31dd
Siraj <ss1425306@gmail.com>
2024-12-19 17:31:58 +0530

    pass data to Admin Dashboard -All task

--- src/Components/Other/AllTask.jsx
+++ src/Components/Other/AllTask.jsx
@@ -1,37 +1,31 @@
-import React from 'react'
+import { useContext } from "react"
+import { AuthContext } from "../../Context/AuthProvider"
 
 const AllTask = () => {
+
+    const authData = useContext(AuthContext)
+    console.log('authData',authData)
+
   return (
-    <div id='alltask' className='bg-[#1c1c1c] p-5 mt-5 h-48 rounded overflow-auto'>
-        
+    <div id='alltask' className='bg-[#1c1c1c] p-5 mt-5'>
         <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>Siraj</h2>
-            <h3>Make UI Design</h3>
-            <h5>Status</h5>
+            <h2 className="text-lg font-medium w-1/5">Name</h2>
+            <h3 className="text-lg font-medium w-1/5">New Task</h3>
+            <h5 className="text-lg font-medium w-1/5">Active Task</h5>
+            <h5 className="text-lg font-medium w-1/5">Completed</h5>
+            <h5 className="text-lg font-medium w-1/5">Failed</h5>
         </div>
-        
-        <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>Siraj</h2>
-            <h3>Make UI Design</h3>
-            <h5>Status</h5>
-        </div>
-        
-        <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>Siraj</h2>
-            <h3>Make UI Design</h3>
-            <h5>Status</h5>
-        </div>
-        
-        <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>Siraj</h2>
-            <h3>Make UI Design</h3>
-            <h5>Status</h5>
-        </div>
-        
-        <div className='bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>Siraj</h2>
-            <h3>Make UI Design</h3>
-            <h5>Status</h5>
+        <div className="">
+            {authData.employees.map((element)=>(
+                <div key={element.id} 
+                className="border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between">
+                    <h2 className="text-lg font-medium w-1/5 text-blue-100">{element.firstName}</h2>
+                    <h3 className="text-lg font-medium w-1/5 text-blue-400">{element.taskCounts.newTask}</h3>
+                    <h5 className="text-lg font-medium w-1/5 text-yellow-400">{element.taskCounts.active}</h5>
+                    <h5 className="text-lg font-medium w-1/5 text-green-400">{element.taskCounts.completed}</h5>
+                    <h5 className="text-lg font-medium w-1/5 text-red-400">{element.taskCounts.failed}</h5>
+                </div>
+            ))}
         </div>
     </div>
   )
--- src/Components/Other/Header.jsx
+++ src/Components/Other/Header.jsx
@@ -1,16 +1,15 @@
-import Login from "../Auth/Login"
 
 const Header = () => {
-
   const logOut= ()=>{
     localStorage.setItem('loggedInUser','')
+    window.location.reload()
   }
 
   return (
     <div className='flex items-center justify-between'>
         <h1 className='text-2xl font-medium text-left flex flex-col'>
             Hello
-            <span className='text-3xl font-semibold'>{'Admin'} 👋</span>
+            <span className='text-3xl font-semibold'>{'username'} 👋</span>
         </h1>
         <button onClick={logOut}
         className='bg-red-600 text-lg font-medium text-white px-3 py-1 sm:px-5 sm:py-2 rounded-sm'>Logout</button>
